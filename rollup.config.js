075a7a52e473cb143aad52af97b0949baae82639
Anton Dalgren <anton@84codes.com>
2026-02-19 13:06:42 +0100

    Add missing Node builtins to rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 export default {
   input: 'dist/sparoid.js',
-  external: ['dgram', 'dns', 'crypto', 'buffer', 'process', 'net'],
+  external: ['dgram', 'dns', 'dns/promises', 'crypto', 'buffer', 'process', 'net', 'os'],
   output: {
     file: 'dist/sparoid.cjs',
     format: 'cjs',
